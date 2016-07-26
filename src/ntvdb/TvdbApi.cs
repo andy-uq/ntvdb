068879d4de513b200f21257d0b9d24476c09716b
Andy Clarke <andy.uq@gmail.com>
2016-07-27 07:31:06 +1200

    Update Tvdb API calls

--- src/ntvdb/TvdbApi.cs
+++ src/ntvdb/TvdbApi.cs
@@ -1,18 +1,20 @@
 using System;
 using System.IO;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using Nito.AsyncEx;
 
 namespace NTvdb
 {
 	public interface ITvdbApi
 	{
-		Task<TvdbSeriesSearchResult> SearchSeriesAsync(string name, CancellationToken cancellationToken);
+		Task<TvdbSeriesSearchResult[]> SearchSeriesAsync(string name, CancellationToken cancellationToken);
 		Task<TvdbSeries> GetSeries(int seriesId, CancellationToken cancellationToken);
 	}
 
@@ -41,36 +43,55 @@ namespace NTvdb
 	public class TvdbApi : ITvdbApi
 	{
 		private readonly AsyncLazy<HttpClient> _httpClient;
+		private readonly JsonSerializer _serialiser;
 
 		public TvdbApi(string baseUrl, TvdbKeys tvdbKeys)
 		{
 			_httpClient = new AsyncLazy<HttpClient>(() => CreateTvdbClient(baseUrl, tvdbKeys));
+			_serialiser = new JsonSerializer {ContractResolver = new CamelCasePropertyNamesContractResolver()};
 		}
 
-		public async Task<TvdbSeriesSearchResult> SearchSeriesAsync(string name, CancellationToken cancellationToken)
+		public async Task<TvdbSeriesSearchResult[]> SearchSeriesAsync(string name, CancellationToken cancellationToken)
 		{
-			var search = new TvdbSearchRequest(name);
-			var request = new HttpRequestMessage(HttpMethod.Post, "/search/series")
-			{
-				Content = new StringContent(JsonConvert.SerializeObject(search))
-			};
+			name = Uri.EscapeDataString(name);
 
-			var response = await SendAsync(request, cancellationToken);
-			response.EnsureSuccessStatusCode();
-
-			var result = await response.Content.ReadAsJsonAsync<SearchResultSchema>();
-			return new TvdbSeriesSearchResult(result);
+			var result = await SendAsync<SearchResultSchema[]>(() => new HttpRequestMessage(HttpMethod.Get, $"/search/series?name={name}"), cancellationToken);
+			return result.Select(r => new TvdbSeriesSearchResult(r)).ToArray();
 		}
 
 		public async Task<TvdbSeries> GetSeries(int seriesId, CancellationToken cancellationToken)
 		{
-			var request = new HttpRequestMessage(HttpMethod.Get, $"/series/{seriesId}");
-
-			var response = await SendAsync(request, cancellationToken);
-			response.EnsureSuccessStatusCode();
-
-			var result = await response.Content.ReadAsJsonAsync<SeriesDetailsSchema>();
+			var result = await SendAsync<SeriesDetailsSchema>(() => new HttpRequestMessage(HttpMethod.Get, $"/series/{seriesId}"), cancellationToken);
 			return new TvdbSeries(result);
+		}
+
+		private HttpRequestMessage BuildPostRequest(object content, string relativeUrl)
+		{
+			var stream = new MemoryStream();
+			var writer = new JsonTextWriter(new StreamWriter(stream));
+			_serialiser.Serialize(writer, content);
+
+			var httpContent = new StreamContent(stream) { Headers = { ContentType = MediaTypeHeaderValue.Parse("application/json") } };
+			return new HttpRequestMessage(HttpMethod.Post, relativeUrl)
+			{
+				Content = httpContent
+			};
+		}
+
+		private async Task<T> SendAsync<T>(Func<HttpRequestMessage> requestBuilder, CancellationToken cancellationToken)
+		{
+			using (var request = requestBuilder())
+			{
+				using (var response = await SendAsync(request, cancellationToken))
+				{
+					response.EnsureSuccessStatusCode();
+
+					var schema = new {data = default(T)};
+
+					var result = await response.Content.ReadAsAnonymousJsonAsync(schema);
+					return result.data;
+				}
+			}
 		}
 
 		private async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
@@ -95,9 +116,13 @@ namespace NTvdb
 
 		private async Task<string> GetJwtToken(TvdbKeys tvdbKeys, HttpClient client)
 		{
+			var loginJson = JsonConvert.SerializeObject(new { apikey = tvdbKeys.ApiKey, username = tvdbKeys.Username, userkey = tvdbKeys.UserKey });
 			var loginRequest = new HttpRequestMessage(HttpMethod.Post, "/login")
 			{
-				Content = new StringContent(JsonConvert.SerializeObject(tvdbKeys), Encoding.ASCII, "application/json")
+				Content = new StringContent(loginJson)
+				{
+					Headers = { ContentType = MediaTypeHeaderValue.Parse("application/json") }
+				}
 			};
 
 			var response = await client.SendAsync(loginRequest);
